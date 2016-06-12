36824d204fe4bf8868f48b644cc90d5715dc7dbb
black <v.cherednichenko@favbet.com>
2016-06-12 19:23:41 +0300

    - for backup

--- fget.erl
+++ fget.erl
@@ -12,30 +12,33 @@
 
 get(File) when is_list(File) ->
     case file:read_file(File) of
-        {ok, Data} -> parse_flac_header(Data);
+        {ok, Data} -> check_flac_file(Data);
         Any -> Any
     end.
 
-parse_flac_header(<<"fLaC", Rest/binary>>) ->
+check_flac_file(<<"fLaC", Rest/binary>>) ->
     io:format("This is FLAC file ~p~n", [?MODULE]),
     parse_bin(<<Rest/binary>>, [], 0).
 
 parse_bin(Bin, Result, N) ->
     case Bin of
-      <<0:1, Type:7, Length:24, Rest/binary>> ->
-%	io:format("First metadata block is: ~p~n", [[Type, Length]]),
-%	parse_bin(<<Rest/binary>>, <<>>, [], N + 1);
-	cut_length(0, <<Rest/binary>>, Type, Length, Result);
-      <<1:1, Type:7, Length:24, Rest/binary>> ->
-	cut_length(1, <<Rest/binary>>, Type, Length, Result);
-      <<>> ->
-	lists:reverse(Result);
-      _ -> error
+        %% Find some blocks in header
+        <<0:1, Type:7, Length:24, Rest/binary>> ->
+            cut_length(0, <<Rest/binary>>, Type, Length, Result);
+        %% Find last block in header
+        <<1:1, Type:7, Length:24, Rest/binary>> ->
+            cut_length(1, <<Rest/binary>>, Type, Length, Result);
+        <<>> ->
+            lists:reverse(Result);
+        _ -> error
     end.
 
 cut_length(0, Rest, ?BLOCK_TYPE_VORBIS_COMMENT=Type, Length, Result) ->
-    io:format("Block 4 FOUND ..~p~n", [[Type, Length]]),
+    io:format("Block 4 FOUND ...~p~n", [[Type, Length]]),
     <<_Block:Length/binary, BinCutted/binary>> = Rest,
+    %
+    parse_block(<<_Block:Length/binary>>),
+    %
     parse_bin(<<BinCutted/binary>>, [{Type, Length}|Result], 0);
 cut_length(0, Rest, Type, Length, Result) ->
     io:format("Info for cutting ...~p~n", [[Type, Length]]),
@@ -45,3 +48,9 @@ cut_length(1, Rest, Type, Length, Result) ->
     io:format("Last Block for cutting ...~p~n", [[Type, Length]]),
     <<_Block:Length/binary, _BinCutted/binary>> = Rest,
     parse_bin(<<>>, [{Type, Length}|Result], 0).
+
+parse_block(<<_:32, Flac_block/binary>>) ->
+    io:format("As Term: ~w~n, ", [Flac_block]),
+    io:format("As String: ~s~n, ", [Flac_block]),
+    io:format("As Pretty: ~p~n", [[Flac_block, 0]]).
+
