86cc189caaa8f5448abeb445a524971a4a4715dc
black <v.cherednichenko@favbet.com>
2016-07-16 21:20:31 +0300

    - change to map

--- flactags.erl
+++ flactags.erl
@@ -44,10 +44,10 @@ find_blocks(0, File, Offset, Acc) ->
     {ok, Block} = file:read(File, ?BLOCK_HEADER_SIZE),
     {Last, BlockType, BlockLength} = parse_block_header(Block),
     %Result = parse_blocks(BlockType, File, Offset, BlockLength, []),
-    case parse_blocks(BlockType, File, Offset, BlockLength, []) of
-        {ok,Result} ->    
-            find_blocks(Last, File, Offset + BlockLength + ?BLOCK_HEADER_SIZE, [Result|Acc]);
-        {skip,_} ->    
+    case parse_blocks(BlockType, File, Offset, BlockLength, #{}) of
+        {ok,Result} ->
+            find_blocks(Last, File, Offset + BlockLength + ?BLOCK_HEADER_SIZE, [{BlockType, Result}|Acc]);
+        {skip,_} ->
             find_blocks(Last, File, Offset + BlockLength + ?BLOCK_HEADER_SIZE, Acc)
     end;
     %find_blocks(Last, File, Offset + BlockLength + ?BLOCK_HEADER_SIZE, [Result|Acc]);
@@ -56,10 +56,10 @@ find_blocks(1, File, Offset, Acc) ->
     {ok, Block} = file:read(File, ?BLOCK_HEADER_SIZE),
     {_Last, BlockType, BlockLength} = parse_block_header(Block),
     %Result = parse_blocks(BlockType, File, Offset, BlockLength, []),
-    case parse_blocks(BlockType, File, Offset, BlockLength, []) of
-        {ok,Result} ->    
-            [Result|Acc];
-        {skip,_} ->    
+    case parse_blocks(BlockType, File, Offset, BlockLength, #{}) of
+        {ok, Result} ->
+            [{BlockType, Result}|Acc];
+        {skip, _} ->
             Acc
     end.
 
@@ -73,21 +73,22 @@ parse_block_header(<<Last:1, Type:7, Length:24>>) ->
 
 % Block Data Parser
 % There we can add parsing any other blocks
-parse_blocks(?BLOCK_TYPE_VORBIS_COMMENT, File, Offset, BlockLength, _ResAcc) ->
+parse_blocks(?BLOCK_TYPE_VORBIS_COMMENT, File, Offset, BlockLength, TagsMap) ->
     file:position(File, Offset + ?BLOCK_HEADER_SIZE),
     {ok, Block} = file:read(File, BlockLength),
-    parse_block4_vn(<<Block/binary>>, []);
+    parse_block4_vn(<<Block/binary>>, TagsMap);
 parse_blocks(BlockType, _, _, _, _) ->
     {skip, BlockType}.
 
 
-parse_block4_vn(<<VNLen:4/little-signed-integer-unit:8, RestBlock4/binary>>, _List) ->
+parse_block4_vn(<<VNLen:4/little-signed-integer-unit:8, RestBlock4/binary>>, TagsMap) ->
     <<VendorName:VNLen/binary, _Skip:4/binary, _Block4Cutted/binary>> = RestBlock4,
-    parse_block(<<_Block4Cutted/binary>>, [<<"VENDORNAME">>,VendorName]).
+    parse_block(<<_Block4Cutted/binary>>, maps:put(<<"VENDORNAME">>, VendorName, TagsMap)).
 
-parse_block(<<VectorLen:4/little-signed-integer-unit:8, Block4/binary>>, TagsList) ->
+parse_block(<<VectorLen:4/little-signed-integer-unit:8, Block4/binary>>, TagsMap) ->
     <<Tag:VectorLen/binary, TagRest/binary>> = Block4,
-    parse_block(<<TagRest/binary>>, [binary:split(Tag, <<"=">>)|TagsList]);
-parse_block(<<>>, TagsList) ->
-	{ok, TagsList}.
+    [Key, Val] = binary:split(Tag, <<"=">>),
+    parse_block(<<TagRest/binary>>, maps:put(Key, Val, TagsMap));
+parse_block(<<>>, TagsMap) ->
+	{ok, TagsMap}.
 
